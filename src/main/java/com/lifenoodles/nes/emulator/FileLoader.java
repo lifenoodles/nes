d7aefdeba215c866b142736a6d8c3c9f8ad0837d
Donagh Hatton <donaghhatton@gmail.com>
2014-06-07 00:38:20 +0100

    tidy

--- src/main/java/com/lifenoodles/nes/emulator/FileLoader.java
+++ src/main/java/com/lifenoodles/nes/emulator/FileLoader.java
@@ -12,40 +12,40 @@ import java.io.*;
 public class FileLoader {
 
 
-    private boolean isTrainerPresent(byte[] header) {
+    private static boolean isTrainerPresent(byte[] header) {
         assert header.length == 16;
-        return (header[6] & 0b100 ) > 0;
+        return (header[6] & 0b100) > 0;
     }
 
     public static ROM loadROM(File file) throws BadRomException, IOException {
+        if (!file.exists()) {
+            throw new IOException();
+        }
 
-        FileInputStream fr = new FileInputStream(file);
-        BufferedInputStream bis = new BufferedInputStream(fr);
-
-        if (!file.exists()) throw IOException;
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream =
+                new BufferedInputStream(fileInputStream);
 
         byte[] headerArray = new byte[16];
+        int headerCode = bufferedInputStream.read(headerArray, 0, 16);
 
-        int headerCode = bis.read(headerArray 0, 16);
-
-        if headerCode != 16 throw BadRomException;
+        if (headerCode != 16) {
+            throw new BadRomException();
+        }
 
         // if  0-3: Constant $4E $45 $53 $1A ("NES" followed by MS-DOS end-of-file)
-        if (headerArray[0] == 0x4E && headerArray[1] == 0x45 && headerArray[2] == 0x53 && headerArray[3] == 0x1A) {
-            if (isTrainerPresent()) {
+        if (headerArray[0] == 0x4E &&
+                headerArray[1] == 0x45 &&
+                headerArray[2] == 0x53 &&
+                headerArray[3] == 0x1A) {
+            if (isTrainerPresent(headerArray)) {
                 byte[] trainerArray = new byte[512];
             } else {
                 byte[] trainerArray = new byte[0];
             }
+        } else {
+            throw new BadRomException("Incorrect byte values in header.");
         }
-        else {
-            system.out.println("Incorrect byte values in header");
-            throw BadRomException;
-        }
-
-
         return new ROM();
     }
-
-
 }
--- src/main/java/com/lifenoodles/nes/exceptions/BadRomException.java
+++ src/main/java/com/lifenoodles/nes/exceptions/BadRomException.java
@@ -5,4 +5,9 @@ package com.lifenoodles.nes.exceptions;
  *         created on 06/06/2014.
  */
 public class BadRomException extends Exception {
+    public BadRomException() {}
+
+    public BadRomException(String message) {
+        super(message);
+    }
 }
