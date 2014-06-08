5b16adb7f8799650d0678ef1c0e4d793cd819b14
Donagh Hatton <donaghhatton@gmail.com>
2014-06-08 13:40:54 +0100

    EmulatorController and VirtualNes added

---
+++ src/main/java/com/lifenoodles/nes/controller/EmulatorController.java
@@ -0,0 +1,36 @@
+package com.lifenoodles.nes.controller;
+
+import com.lifenoodles.nes.emulator.VirtualNes;
+
+/**
+ * Base class for anything that controls emulator execution.
+ * The intention is that the controller can manage instruction execution
+ * so that the apparent speed of the emulation is correct.
+ * @author Donagh Hatton
+ *         created on 08/06/2014.
+ */
+public abstract class EmulatorController implements Runnable {
+    private boolean isRunning = false;
+    private VirtualNes vnes;
+
+    public EmulatorController() {
+        vnes = new VirtualNes();
+    }
+
+    /**
+     * Get the running status of the controlled VNES
+     * @return a boolean indicating whether the controlled VNES is running
+     */
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    /**
+     * Start or stop the controlled VNES
+     * @param isRunning boolean
+     */
+    public synchronized void setRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+}
+
---
+++ src/main/java/com/lifenoodles/nes/emulator/VirtualNes.java
@@ -0,0 +1,10 @@
+package com.lifenoodles.nes.emulator;
+
+/**
+ * Main class for representing the NES h/w, contains methods for managing
+ * execution flow of the cpu and provides connectivity for modules
+ * @author Donagh Hatton
+ *         created on 08/06/2014.
+ */
+public class VirtualNes {
+}
