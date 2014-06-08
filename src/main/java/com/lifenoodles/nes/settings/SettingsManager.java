3b415896a726adf8c2cc73551254e232433dee2e
Donagh Hatton <donaghhatton@gmail.com>
2014-06-08 16:37:19 +0100

    settings tester added

--- src/main/java/com/lifenoodles/nes/settings/SettingsManager.java
+++ src/main/java/com/lifenoodles/nes/settings/SettingsManager.java
@@ -1,6 +1,7 @@
 package com.lifenoodles.nes.settings;
 
 /**
+ * Manager for settings files
  * @author Cian Hatton
  *         created on 08/06/2014.
  */
---
+++ src/test/java/com/lifenoodles/nes/settings/SettingsManagerTester.java
@@ -0,0 +1,11 @@
+package com.lifenoodles.nes.settings;
+
+import junit.framework.TestCase;
+
+/**
+ * Test cases for the Settings Manager
+ * @author Cian Hatton
+ *         created on 08/06/2014.
+ */
+public class SettingsManagerTester extends TestCase {
+}
