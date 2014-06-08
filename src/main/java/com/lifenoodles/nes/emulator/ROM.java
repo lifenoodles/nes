8f56f4b7cf0feb8f97c0b2b93c1dc5983c7bb152
Donagh Hatton <donaghhatton@gmail.com>
2014-06-08 18:28:46 +0100

    initial rom data added to rom

--- src/main/java/com/lifenoodles/nes/emulator/ROM.java
+++ src/main/java/com/lifenoodles/nes/emulator/ROM.java
@@ -3,10 +3,25 @@ package com.lifenoodles.nes.emulator;
 /**
  * Representation of loaded ROM
  */
-public class ROM
-{
-    public ROM()
-    {
+public class ROM {
+    private Mapper mapper;
+    public byte[] header;
+    public byte[] prgRom;
+    public byte[] chrRom;
 
+    public ROM(byte[] header, byte[] prgRom, byte[] chrRom) {
+        this.header = new byte[header.length];
+        this.prgRom = new byte[prgRom.length];
+        this.chrRom = new byte[chrRom.length];
+        System.arraycopy(header, 0, this.header, 0, header.length);
+        System.arraycopy(prgRom, 0, this.prgRom, 0, prgRom.length);
+        System.arraycopy(chrRom, 0, this.chrRom, 0, chrRom.length);
+        this.mapper = Mapper.fromCode(extractMapper(this.header));
+    }
+
+    private int extractMapper(byte[] header) {
+        int lowerMapper = header[6] & 0xF0;
+        int upperMapper = header[7] & 0xF0;
+        return (upperMapper << 4) | lowerMapper;
     }
 }
