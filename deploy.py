b6dcb5dc66541d8f6468a54768c6c670b172e04b
Donagh Hatton <donaghhatton@gmail.com>
2014-07-03 17:42:52 +0100

    actually added the script this time

---
+++ deploy.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Build script for the NES emulator, Maven is sort of shitty
+for tasks like copying files etc. so we'll just use this instead.
+"""
+
+import os
+import shutil
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    # run the maven build
+    maven_result = subprocess.call(["mvn", "compile"])
+    if maven_result != 0:
+        print("Error occured during build process, aborting.")
+        sys.exit(1)
+
+    # set up release directory and copy shit into it
+    try:
+        os.mkdir("release")
+    except:
+        pass
+
+    shutil.copy(
+        os.path.join(
+            "target", "nes-0.1-SNAPSHOT-jar-with-dependencies.jar"),
+        os.path.join("release", "nes-emulator.jar"))
+
+    shutil.copy(
+        os.path.join("scripts", "nes.sh"), "release")
+
+    shutil.copytree(
+        os.path.join(
+            "target", "natives"),
+        os.path.join("release", "natives"))
