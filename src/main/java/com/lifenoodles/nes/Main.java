e58ad3996e0ff989abb7235b893b9ca6ac3da9ea
Donagh Hatton <donaghhatton@gmail.com>
2014-08-16 00:52:31 +0100

    optionParser included

--- src/main/java/com/lifenoodles/nes/Main.java
+++ src/main/java/com/lifenoodles/nes/Main.java
@@ -1,13 +1,29 @@
 package com.lifenoodles.nes;
 
 import com.lifenoodles.nes.ui.MainForm;
+import org.lifenoodles.jargparse.HelpfulOptionParser;
+import org.lifenoodles.jargparse.Option;
+import org.lifenoodles.jargparse.OptionSet;
 
 /**
  * Hello world!
  */
 public class Main {
     public static void main(String[] args) {
-        MainForm form = new MainForm();
-        form.show();
+        HelpfulOptionParser parser = new HelpfulOptionParser()
+                .setApplicationName("nes");
+        parser.addOption(Option.optional("-r").alias("--rom").make());
+        parser.addOption(Option.optional("-t").alias("--test").arguments(0)
+                .make());
+        parser.addOption(Option.optional("-d").alias("--debug").arguments(0)
+                .make());
+        OptionSet optionSet = parser.parse(args);
+
+        if (optionSet.contains("--test")) {
+            // do something testy
+        } else {
+            MainForm form = new MainForm();
+            form.show();
+        }
     }
 }
