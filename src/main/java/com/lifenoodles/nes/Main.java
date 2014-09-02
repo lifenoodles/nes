586572d7102ec079fc6c2f5f12dd10f61368f53c
Donagh Hatton <donaghhatton@gmail.com>
2014-09-02 21:03:58 +0100

    fixed for new jargparse api

--- src/main/java/com/lifenoodles/nes/Main.java
+++ src/main/java/com/lifenoodles/nes/Main.java
@@ -1,7 +1,7 @@
 package com.lifenoodles.nes;
 
 import com.lifenoodles.nes.ui.MainForm;
-import org.lifenoodles.jargparse.HelpfulOptionParser;
+import org.lifenoodles.jargparse.AutoOptionParser;
 import org.lifenoodles.jargparse.Option;
 import org.lifenoodles.jargparse.OptionSet;
 
@@ -10,15 +10,11 @@ import org.lifenoodles.jargparse.OptionSet;
  */
 public class Main {
     public static void main(String[] args) {
-        HelpfulOptionParser parser = new HelpfulOptionParser()
-                .setApplicationName("nes");
-        parser.addOption(Option.optional("-r").alias("--rom").make());
-        parser.addOption(Option.optional("-t").alias("--test").arguments(0)
-                .make());
-        parser.addOption(Option.optional("-d").alias("--debug").arguments(0)
-                .make());
-        OptionSet optionSet = parser.parse(args);
-
+        OptionSet optionSet = new AutoOptionParser("nes")
+                .add(Option.of("-r", "--rom").arguments(1))
+                .add(Option.of("-t", "--test"))
+                .add(Option.of("-d", "--debug"))
+                .parse(args);
         if (optionSet.contains("--test")) {
             // do something testy
         } else {
